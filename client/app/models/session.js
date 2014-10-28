ddb71f545d6f0a49f3fa80eea75a9a6f7a5d0366
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2014-10-28 15:36:08 +0100

    add: added remaining client

---
+++ client/app/models/session.js
@@ -0,0 +1,51 @@
+import DS from 'ember-data';
+
+//////////////////////////////////
+////// LIVE DATA /////////////////
+//////////////////////////////////
+
+/*export default DS.Model.extend({
+  title: DS.attr('string'),
+  presenter: DS.attr('string'),
+  description: DS.attr('string'),
+  area: DS.attr(),
+  rating: DS.attr('number'),
+  recommendation: DS.attr('number')
+});*/
+
+///////////////////////////////////
+/////////// FIXTURE DATA //////////
+///////////////////////////////////
+var Session = DS.Model.extend({
+	title: DS.attr('string'),
+  presenter: DS.attr('string'),
+  description: DS.attr('string'),
+  //area: DS.attr(),
+  rating: DS.attr('number'),
+  recommendation: DS.attr('number')
+});
+
+Session.reopenClass({
+	FIXTURES: [
+		{
+			id: 1,
+			title: "A word on notes",
+			presenter: "Fjodor Ekström",
+			description: "How should you really use notes and what is words anyway?",
+			//area: ["web","iot","philosophy"],
+			rating: 5,
+			recommendation: 2
+		},
+		{
+			id: 2,
+			title: "A word on pigs",
+			presenter: "Fjodor Ekström",
+			description: "How should you really use pigs and what is stables anyway?",
+			//area: ["farming","human survival","self sufficiency"],
+			rating: 2,
+			recommendation: 7
+		}
+	]
+});
+
+export default Session;
---
+++ client/app/routes/session.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model:function(params){
+		return this.store.find('session', params.session_id);
+	},
+	serialize: function(session){
+		return {session_id: session.get('id')};
+	}
+});

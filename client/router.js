4a48fe9703d64bfe74c02feb7efac4f2e0d7d4fd
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2014-10-28 15:33:18 +0100

    add: added client

---
+++ client/router.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.resource('sessions', {path: '/sessions'});
+  this.resource('session', {path: '/sessions/:session_id'});
+});
+
+export default Router;
